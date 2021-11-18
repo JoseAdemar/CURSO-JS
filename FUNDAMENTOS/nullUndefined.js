87c8c2100cea0d75aec2c1f94a9fa2ee1ec93c0a
jose_ademar <joseademar.junior@shift.com.br>
2021-11-17 22:30:49 -0300

    Criando objetos em JavaScript

---
+++ FUNDAMENTOS/nullUndefined.js
@@ -0,0 +1,5 @@
+let valor // não inicializada vai ser undefined
+console.log(valor)
+
+valor = null // Ausência de valor
+console.log(valor)
---
+++ FUNDAMENTOS/objeto.js
@@ -0,0 +1,21 @@
+// Par de chaves {} é uma forma literal de criar objetos em JavaScript
+
+
+// Forma 01 de criar objetos em JavaScript
+const prod1 = {}
+
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40 // Evitar atributos com espaço
+
+console.log(prod1)
+
+// Forma 02 de criar objetos em JavaScript
+const prod2 = {
+
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    
+}
+
+console.log(prod2)
